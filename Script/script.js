291a8dc45c3e28c86ae714fa246a72a7a7995038
MD. TAUQIR HASAN <58160899+Tauqir129@users.noreply.github.com>
2022-08-17 22:10:08 +0600

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -2,11 +2,3 @@ function scrollWin() {
     window.scrollTo(0, 500);
     console.log('pagescrolled');
   }
-function size{
-    document.getElementById("demo").innerHTML =
-"Screen Width: " + screen.width;
-    document.getElementById("demo1").innerHTML =
-"Screen Width: " + screen.width;
-    console.log("size masured!");
-
-}
